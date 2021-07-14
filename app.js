cd1ad1864329e97fe140ae336dbd99633ad1feb2
1998dobiasz <62941429+1998dobiasz@users.noreply.github.com>
2021-07-14 10:41:22 +0200

    Update app.js
    fix semicolon

--- app.js
+++ app.js
@@ -150,13 +150,13 @@ app.get('/', (req, res) => {
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
-})
+});
 
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
     if (!err.message) err.message = 'Oh No, Something Went Wrong!';
     res.status(statusCode).render('error', { err });
-})
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
